f3955fd1ab8b7d97295d750a0cb9840120670334
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-02-27 15:47:17 -0600

    Working on Project, completed Task 2.

--- index.js
+++ index.js
@@ -76,6 +76,15 @@ class Person {
 */
 
 class Car {
+  constructor (model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill (gallons){
+    return this.tank + gallons
+  }
 
 }
 
